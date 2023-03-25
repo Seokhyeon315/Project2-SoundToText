df2afa7f27426465cfa638eea5d05998f6c42063
Seokhyeon Byun <shyeon0315@gmail.com>
2023-03-25 19:33:25 +0900

    edit app.py but still get error

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+from io import BytesIO
 import os
+import openai
 from pytube import YouTube
+from pydub import AudioSegment
+from tempfile import NamedTemporaryFile
 import argparse
 
 
@@ -25,20 +29,10 @@ def main():
     VIDEO_LINK=user_input
     yt=YouTube(VIDEO_LINK) # Assign YouTube object
 
-    # Extract sound from video and download it
-    audio_stream=yt.streams.filter(only_audio=True).first() #excludes streams with audio tracks
-    filesize=audio_stream.filesize
-
-    # Set ouput path
-    output_path=os.path.join(os.getcwd(), 'audio folder')
-
-    #Create directory if it doesn't exist
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
-
-
-    # Download audio stream to the output path
-    audio_stream.download(output_path=output_path, filename=f"{yt.title}.mp3") #유튜브 제목대로 음성파일 다운로드하게함.
+    # extract audio from video
+    with NamedTemporaryFile() as temp_audio_file:
+        audio_stream = yt.streams.filter(only_audio=True).first().stream_to_buffer(temp_audio_file)
+        audio_size = temp_audio_file.tell()
 
 
     # Convert length of video from seconds to hours, minutes, and seconds
@@ -47,23 +41,52 @@ def main():
     length_minutes = (length_seconds % 3600) // 60
     length_seconds = length_seconds % 60
 
-    # Print video title and length in hours, minutes, and seconds
+    # Print video title, length, file size in bytes and megabytes
+    print(f"User input link: {user_input}")
     print(f"The title: {yt.title}")
     print(f"The length of the video: {length_hours} hrs {length_minutes} mins {length_seconds} secs")
+    print(f"The file size is {audio_size} bytes")
+    print(f"And it is {audio_size/1000000} MB")
+
     # 1MB=1000000bytes
 
-    # check file size to handle with openai api
-    if filesize < 25:
+    # check file size to handle with openai api, 기준 25MB
+    if audio_size < 25000000:
         print("It's less than 25MB, Good size")
+
+        transcribe(temp_audio_file.name)
     else:
         print("Filesize is over 25MB, Not okay")
+        # chunk function 실행
 
 
-    print(f"User input: {user_input}")
-    
-  
-        
-    
+
+# Chunk audio file function
+
+# def chunk():
+#     pass
+
+
+
+
+
+# Trascribe function
+
+def transcribe(audio_file_path):
+    # Load your API key from an environment variable or secret management service
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    with open(audio_file_path, 'rb') as f:
+        transcript = openai.Audio.transcribe("whisper-1", f)
+        print(transcript)
+
+
+
+
+# # Translate function: 98개의 서로다른 언어를 영어로 번역가능
+
+# def translate():
+#     pass
 
 
 
